7960362a5bb52b2b9f89dac1250ffa2e5ba29bdc
Arif Billah <98398506+ArifBillah6482@users.noreply.github.com>
2022-11-08 19:19:31 +0600

    Update Admin.jsx

--- src/Routes/Admin.jsx
+++ src/Routes/Admin.jsx
@@ -2,13 +2,13 @@ import Admin from "../components/Admin";
 
 function AdminRoute() {
      const pass = prompt("");
-    if (pass === "606920") {
+    if (pass === "s") {
   return (
     <div>
       <Admin />
     </div>
   );
-    } else if (pass !== "606920") {
+    } else if (pass !== "s") {
    return <>এখানে কোনো তথ্য নেই।</>;
  } else {
     return <>এখানে কোনো তথ্য নেই।</>;
