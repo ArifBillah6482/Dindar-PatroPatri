ce34252baa291bed5098b98f784682e7cba59825
Arif Billah <98398506+ArifBillah6482@users.noreply.github.com>
2022-11-20 18:29:52 +0600

    Update Admin.jsx

--- src/Routes/Admin.jsx
+++ src/Routes/Admin.jsx
@@ -2,13 +2,13 @@ import Admin from "../components/Admin";
 
 function AdminRoute() {
      const pass = prompt("Enter your eight digit password: ");
-    if (pass === process.env.PASS) {
+    if (pass === process.env.REACT_APP_TOKEN) {
   return (
     <div>
       <Admin />
     </div>
   );
-    } else if (pass != process.env.PASS) {
+    } else if (pass !== process.env.REACT_APP_TOKEN) {
    return <p style={{color: 'red', margin: '20px'}}>পাসওয়ার্ড ভুল!</p>;
  } else {
     return <p style={{color: 'red', margin: '20px'}}>পাসওয়ার্ড ভুল!</p>;
